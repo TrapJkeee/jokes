dc1361aa03dd9785f39659c5e12b151ee41d763a
Sergei Shatohin <trapjkeeee@gmail.com>
2023-03-14 14:32:43 +0300

    реализация работы с сервером, реализация добавления комментариев

--- src/pages/JokeDetails.js
+++ src/pages/JokeDetails.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedJoke from "../components/jokes/HighlightedJoke";
-
-const DUMMY_JOKES = [
-  { id: "j1", topic: "Programming", text: "wytka 1" },
-  { id: "j2", topic: "General", text: "wytka 2" },
-];
+import useHttp from "../hooks/use-http";
+import { getJoke } from "../utils/firebase-api";
+import Loader from "../components/UI/Loader";
 
 const JokeDetails = () => {
   const params = useParams();
   const routeMatch = useRouteMatch();
 
-  const joke = DUMMY_JOKES.find((joke) => joke.id === params.jokeId);
+  const {
+    sendHttpRequest,
+    status,
+    data: loadedJoke,
+    error,
+  } = useHttp(getJoke, true);
 
-  if (!joke) {
-    return <h1 className="centered">Wytok ne nideno</h1>;
+  useEffect(() => {
+    sendHttpRequest(params.jokeId);
+  }, [sendHttpRequest, params.jokeId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <Loader />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (!loadedJoke.text) {
+    return <h1 className="centered">Joke not found</h1>;
   }
 
   return (
     <>
-      <HighlightedJoke topic={joke.topic} text={joke.text} />
+      <HighlightedJoke topic={loadedJoke.topic} text={loadedJoke.text} />
 
       <Route path={`${routeMatch.path}`} exact>
         <div className="centered">
--- src/pages/Jokes.js
+++ src/pages/Jokes.js
@@ -1,12 +1,38 @@
+import { useEffect } from "react";
 import JokeList from "../components/jokes/JokeList";
-
-const DUMMY_JOKES = [
-  { id: "j1", topic: "Programming", text: "wytka 1" },
-  { id: "j2", topic: "General", text: "wytka 2" },
-];
+import Loader from "../components/UI/Loader";
+import useHttp from "../hooks/use-http";
+import { getJokes } from "../utils/firebase-api";
+import NoJokesFound from "../components/jokes/NoJokesFound";
 
 const Jokes = () => {
-  return <JokeList jokes={DUMMY_JOKES} />;
+  const {
+    sendHttpRequest,
+    status,
+    data: loadedJokes,
+    error,
+  } = useHttp(getJokes, true);
+
+  useEffect(() => {
+    sendHttpRequest();
+  }, [sendHttpRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <Loader />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (status === "completed" && (!loadedJokes || loadedJokes.length === 0)) {
+    return <NoJokesFound />;
+  }
+  return <JokeList jokes={loadedJokes} />;
 };
 
 export default Jokes;
